6d5a5961398c1351faf1bfaaf374151ad46b87aa
Teon Ooi <teon@protonmail.com>
2023-08-25 23:11:19 +0800

    Refactor task filter into component.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script>
+import TaskFilter from './components/TaskFilter.vue'
+
 export default {
+    components: {
+        TaskFilter
+    },
     data() {
         return {
             tasks: [],
             task: '',
             filter: '',
             error: '',
+            receivedValue: '',
         }
     },
     methods: {
@@ -57,6 +63,9 @@ export default {
         validateTask( task ) {
             return task.length > 0;
         },
+        test( a ) {
+            console.log(a);
+        }
     },
 }
 </script>
@@ -82,34 +91,24 @@ export default {
             <div class="d-flex align-items-center mt-4">
                 <h4 class="flex-fill m-0">Tasks</h4>
 
+                {{ receivedValue }}
+
                 <ul class="nav nav-underline flex-fill justify-content-end">
-                    <li class="nav-item">
-                        <a @click.prevent="filter=''" :class="{ 'active': filter == '' }" class="nav-link" href="#">
-                            All ({{ countTasks() }})
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a @click.prevent="filter='pending'" :class="{ 'active': filter == 'pending' }" class="nav-link" href="#">
-                            Pending ({{ countTasks( 'pending' ) }})
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a @click.prevent="filter='done'" :class="{ 'active': filter == 'done' }" class="nav-link" href="#">
-                            Done ({{ countTasks('done' ) }})
-                        </a>
-                    </li>
+                    <TaskFilter @select="filter = $event" :counter="countTasks" :active=filter type="all" />
+                    <TaskFilter @select="filter = $event" :counter="countTasks" :active=filter type="pending" />
+                    <TaskFilter @select="filter = $event" :counter="countTasks" :active=filter type="done" />
                 </ul>
             </div>
 
             <div v-show="countTasks( filter )" class="list-group mt-3">
                 <div v-for="task in getTasks( filter )" class="list-group-item list-group-item-action d-flex align-items-center">
-                    <a @:click.prevent="editTask( task.id )" :class="[ task.done ? 'btn-outline-success' : 'btn-outline-secondary text-white' ]" class="btn btn-sm me-2">
+                    <a @click.prevent="editTask( task.id )" :class="[ task.done ? 'btn-outline-success' : 'btn-outline-secondary text-white' ]" class="btn btn-sm me-2">
                         <font-awesome-icon icon="fa-solid fa-check" />
                     </a>
 
                     <p :class="{ 'text-decoration-line-through': task.done }" class="flex-grow-1 mb-0 text-secondary">{{ task.label }}</p>
 
-                    <a @:click.prevent="deleteTask( task.id )" class="btn btn-sm text-danger">
+                    <a @click.prevent="deleteTask( task.id )" class="btn btn-sm text-danger">
                         <font-awesome-icon icon="fa-solid fa-trash" />
                     </a>
                 </div>
---
+++ src/components/TaskFilter.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    props: [
+        'type',
+        'active',
+        'counter',
+    ],
+}
+</script>
+
+<template>
+<li class="nav-item">
+    <a @click.prevent="$emit( 'select', type )" :class="{ 'active': active == type }" class="nav-link" href="#">
+        {{ type }} ({{ counter( type ) }})
+    </a>
+</li>
+</template>
